64d4cf7039f8ddeb60575326949e2e40a3c5e50a
Sex thủ si tềnh <khoa.ngkhoa26@gmail.com>
2023-07-18 15:20:36 +0700

    Menu đang hoàn thành

--- frontend/src/components/Menu.tsx
+++ frontend/src/components/Menu.tsx
@@ -1,15 +1,32 @@
+import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
+import ButtonHidden from "./ButtonHidden";
+
 export default function Menu(): JSX.Element {
   return (
-    <nav className="flex">
-      <div className="flex justify-start">
-        <a href="/">Trang chủ</a>
-        <a href="/product">Cửa hàng</a>
+
+ <div className='flex justify-between'> 
+      <div className="flex">
+            <a href="">
+                  <img src="/asset/logo.jpg" alt="Logo" className="w-28" />
+            </a>
+            <ul className='list-none flex ml-20 items-center uppercase'>
+                  <li className="px-5 hover:text-cyan-400">Home</li>
+                  <li className="px-5 hover:text-cyan-400">Products</li>
+                  <li className="px-5 hover:text-cyan-400">About</li>
+                  <li className="px-5 hover:text-cyan-400">New</li>
+            </ul>
       </div>
-      <div className="flex justify-end">
-        <a href="/login" className="text-rose-500 hover:bg-rose-500 hover:text-white">Đăng Nhập</a>
-        <div className="px-2">/</div>
-        <a href="/register" className="text-rose-500 hover:bg-rose-500 hover:text-white">Đăng Ký</a>
-      </div>
-    </nav>
+      <div className="flex mr-5 items-center">
+            <SearchBar />  
+            <a href="">
+                  <img src="/asset/icon-cart.png" alt="Logo"  className="w-14" />
+            </a>  
+            <a href={"login"}  className="px-3 py-2 bg-blue-light rounded-3xl ml-4 hover:text-neutral-50">Đăng nhập</a>
+            <a href={"register"}  className="px-3 py-2 bg-blue-light rounded-3xl ml-4 hover:text-neutral-50">Đăng ký</a>
+            <ButtonHidden />
+      </div>           
+</div>
+
   )
 }
--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,5 +1,5 @@
 export default function SearchBar(): JSX.Element {
   return (
-    <div></div>
+    <input className="border-2 rounded-md border-sky-500 w-60 h-9 px-2 placeholder:italic placeholder:text-slate-400 " placeholder="Bạn đang tìm sản phẩm..." /> 
   )
 }
