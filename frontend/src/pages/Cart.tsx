150b262e25863d54dbbf29ccd48bd9b482f077f7
Sex thủ si tềnh <127826830+Khoadz299@users.noreply.github.com>
2023-08-09 20:27:25 +0700

    Sửa cart page (#27)
    * Sửa lỗi hiển thị data của Icon User
    
    * Edit UI , Product Detail , Load color size
    
    * Edit UI Cart Page

--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,79 +1,97 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router"
-import { v4 as uuidv4 } from "uuid"
-import Cookies from "js-cookie"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { v4 as uuidv4 } from "uuid";
+import Cookies from "js-cookie";
 
-import Menu from "../components/Menu"
-import Footer from "../components/Footer"
+import Menu from "../components/Menu";
+import Footer from "../components/Footer";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface ApiResponse {
-  message: string
+  message: string;
 }
 
 function allStorage(): (string | null)[] {
   var values: (string | null)[] = [],
     keys = Object.keys(localStorage),
-    i = keys.length
+    i = keys.length;
 
   while (i--) {
-    values.push(localStorage.getItem(keys[i]))
+    values.push(localStorage.getItem(keys[i]));
   }
 
-  return values
+  return values;
 }
 
 function isNullOrUndefined(value: any): boolean {
   return value === null || value === undefined;
 }
 
-async function getApiSession(username: string, token: string): Promise<ApiResponse> {
+async function getApiSession(
+  username: string,
+  token: string
+): Promise<ApiResponse> {
   try {
-    const response = await axios.get(`http://localhost:4505/api/account/${username}?token=${token}`)
-    return response.data
+    const response = await axios.get(
+      `http://localhost:4505/api/account/${username}?token=${token}`
+    );
+    return response.data;
   } catch (error) {
-    throw new Error("Can't get data")
+    throw new Error("Can't get data");
   }
 }
 
 function CartPayment(): JSX.Element {
-  const navigate = useNavigate()
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
-  const [products, setProducts] = useState<Object[]>([])
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [products, setProducts] = useState<Object[]>([]);
+  var totalMoney = 0;
 
   const handleLoggedIn = async (value: boolean) => {
-    setLoggedIn(value)
-    const username = Cookies.get("loginUsernameCookie")
+    setLoggedIn(value);
+    const username = Cookies.get("loginUsernameCookie");
     await axios.patch(`http://localhost:4505/api/account/${username}`, {
       active: -1,
       token: "",
-    })
-    Cookies.remove("loginTokenCookie")
-    Cookies.remove("loginUsernameCookie")
-  }
+    });
+    Cookies.remove("loginTokenCookie");
+    Cookies.remove("loginUsernameCookie");
+  };
 
-  const handleDeleteAllCart = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault()
+  const handleDeleteAllCart = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
 
-    localStorage.clear()
-    setProducts([])
-  }
+    localStorage.clear();
+    setProducts([]);
+  };
 
-  const handlePayment = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.preventDefault()
+  const handlePayment = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
 
     if (!loggedIn) {
-      alert("Vui lòng, tạo tài khoản và đăng nhập trước khi tiến hành thanh toán")
-      return
+      alert(
+        "Vui lòng, tạo tài khoản và đăng nhập trước khi tiến hành thanh toán"
+      );
+      return;
     }
 
-    navigate("/payment")
-  }
+    navigate("/payment");
+  };
+
+  const convertMoneyToVND = (money: number) => {
+    return money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  };
 
   useEffect(() => {
     // Get user login
-    const token = Cookies.get("loginTokenCookie")
-    const username = Cookies.get("loginUsernameCookie")
+    const token = Cookies.get("loginTokenCookie");
+    const username = Cookies.get("loginUsernameCookie");
 
     if (!isNullOrUndefined(token) || !isNullOrUndefined(username)) {
       getApiSession(username!, token!)
@@ -83,15 +101,15 @@ function CartPayment(): JSX.Element {
           }
         })
         .catch((error: any) => {
-          console.log("Can't get data from api")
-          console.log(error)
-        })
+          console.log("Can't get data from api");
+          console.log(error);
+        });
     }
 
-    const items = allStorage()
-    const productObjects = items.map((item) => JSON.parse(item || ""))
-    setProducts(productObjects)
-  }, [])
+    const items = allStorage();
+    const productObjects = items.map((item) => JSON.parse(item || ""));
+    setProducts(productObjects);
+  }, []);
 
   return (
     <div className="font-barlow">
@@ -101,55 +119,84 @@ function CartPayment(): JSX.Element {
         <div className="mt-4 py-4 w-full bg-grey-light text-coffee text-center text-2xl font-semibold">
           GIỎ HÀNG
         </div>
-        <table className="mt-10 mb-1w-3/5 px-14 mx-auto">
+        <table className="mt-10 mb-1 w-8/12 px-14 mx-auto">
           <thead>
-            <th className="flex">
-              <td>Sản phẩm</td>
-              <td>Số lượng</td>
-              <td>Tạm tính</td>
-              <td>Xóa</td>
-            </th>
-          </thead>
-          <tbody>
-          {products !== null ?
-          products.map((value: any) => (
-            <tr key={uuidv4()} className="flex">
-              <td className="w-3/5 flex">
-                <img src={`data:image/png;base64,${value.image}`} height={40} width={40} />
-                <p>{value.name}</p>
-              </td>
-              <td>{value.amount}</td>
-              <td>{value.price * value.amount}</td>
-              <td>Xóa</td>
+            <tr>
+              <th className="w-4/6">Sản phẩm</th>
+              <th>Số lượng</th>
+              <th>Tạm tính</th>
+              <th>Xóa</th>
             </tr>
-          ))
-            :
-          <tr>
-            <td ></td>
-          </tr>
-          }
+          </thead>
+
+          <tbody>
+            {products !== null ? (
+              products.map((value: any) => (
+                <tr key={uuidv4()}>
+                  <span className="hidden"> {(totalMoney += value.price)}</span>
+                  <td className="w-3/5 flex mt-5">
+                    <img
+                      src={`data:image/png;base64,${value.image}`}
+                      className="w-2/6"
+                    />
+                    <p className="mx-5">
+                      {value.name} <br /> {convertMoneyToVND(value.price)}
+                    </p>
+                  </td>
+                  <td className="text-center">
+                    <input
+                      className="text-center w-10"
+                      type="number"
+                      min="1"
+                      max="10"
+                      value={value.amount}
+                    />
+                  </td>
+                  <td className="text-center">
+                    {convertMoneyToVND(value.price * value.amount)}
+                  </td>
+                  <td className="text-center">
+                    <FontAwesomeIcon
+                      icon={faTrash}
+                      className=" text-xl text-blue-light hover:cursor-pointer"
+                    />
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td></td>
+              </tr>
+            )}
           </tbody>
-          <tfoot>
-            <th>
-              <td>Sản phẩm</td>
-              <td>Số lượng</td>
-              <td>Tạm tính</td>
-              <td>Xóa</td>
-            </th>
-          </tfoot>
         </table>
       </div>
 
-      <div className="my-10 flex w-1/5 mx-auto">
-        <button className="mx-2 px-8 py-3 bg-coffee text-2xl text-white font-barlow"
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handlePayment(event)}>Thanh toán</button>
-        <button className="mx-2 px-8 py-3 bg-grey-light text-2xl text-coffee font-barlow"
-          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => handleDeleteAllCart(event)}>Xóa tất cả</button>
+      <div className="my-10 flex w-8/12 mx-auto justify-between">
+        <button
+          className="px-8 py-2 bg-coffee text-lg text-white font-barlow"
+          onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+            handlePayment(event)
+          }>
+          Thanh toán
+        </button>
+        <div className="flex">
+          <p className="my-auto mx-4 font-semibold">
+            Tổng tiền tạm tính : {convertMoneyToVND(totalMoney)} VND
+          </p>
+          <button
+            className="px-8 py-2 bg-grey-light text-lg text-coffee font-barlow"
+            onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
+              handleDeleteAllCart(event)
+            }>
+            Xóa tất cả
+          </button>
+        </div>
       </div>
 
       <Footer />
     </div>
-  )
+  );
 }
 
-export default CartPayment
+export default CartPayment;
