263f1543bc1ea86acf6866cf871eddce9353ff45
Sex thủ si tềnh <khoa.ngkhoa26@gmail.com>
2023-08-09 16:19:17 +0700

    Sửa UI Icon User

--- frontend/src/components/UserInfo.tsx
+++ frontend/src/components/UserInfo.tsx
@@ -1,40 +1,49 @@
-import { faUser } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useState } from "react"
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 type UserInfoProps = {
-  loggedIn: boolean
-  handleLoggedIn: (value: boolean) => void
-}
+  loggedIn: boolean;
+  handleLoggedIn: (value: boolean) => void;
+};
 
 export default function UserInfo(props: UserInfoProps): JSX.Element {
-  let component: JSX.Element = <div></div>
-  const [isUserDetail, setUserDetail] = useState<boolean>(false)
+  let component: JSX.Element = <div></div>;
+  const [isUserDetail, setUserDetail] = useState<boolean>(false);
 
   const handleLoggedIn = (value: boolean) => {
-    props.handleLoggedIn(value)
-  }
+    props.handleLoggedIn(value);
+  };
 
   if (props.loggedIn) {
     component = (
       <div>
-        <FontAwesomeIcon icon={faUser} onClick={() => setUserDetail(!isUserDetail)}/>
+        <FontAwesomeIcon
+          className="hover:cursor-pointer h-5 mx-2 text-blue-light"
+          icon={faUser}
+          onClick={() => setUserDetail(!isUserDetail)}
+        />
         <div className={isUserDetail ? "hidden" : ""}>
-          <div >
-            <a href="/userinfo" >Thông tin tài khoản</a>
-            <p className="px-3 py-2 bg-blue-light rounded-3xl ml-4 hover:text-neutral-50" onClick={() => handleLoggedIn(!props.loggedIn)} >Đăng xuất</p>
+          <div className="absolute bg-grey-light right-0 top-16 p-4 rounded-md">
+            <a href="/userinfo">Thông tin tài khoản</a>
+            <p
+              className="px-3 py-2 my-3 text-center text-white bg-blue-light rounded-3xl ml-4 hover:text-black"
+              onClick={() => handleLoggedIn(!props.loggedIn)}>
+              Đăng xuất
+            </p>
           </div>
         </div>
-      </div> 
-    )
+      </div>
+    );
   } else {
     component = (
-      <a href="login-register"
+      <a
+        href="login-register"
         className="px-3 py-2 bg-blue-light rounded-3xl ml-4 hover:text-neutral-50">
         Đăng nhập / Đăng ký
       </a>
-    )
+    );
   }
 
-  return component
+  return component;
 }
