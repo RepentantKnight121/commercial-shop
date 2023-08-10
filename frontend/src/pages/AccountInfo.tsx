eb977076a4151db651af473a468d23561f926de1
Sex thủ si tềnh <127826830+Khoadz299@users.noreply.github.com>
2023-08-10 12:32:38 +0700

    Sửa giao diện account info , payment (#28)

--- frontend/src/pages/AccountInfo.tsx
+++ frontend/src/pages/AccountInfo.tsx
@@ -1,34 +1,34 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { useNavigate } from "react-router"
-import Cookies from "js-cookie"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import Cookies from "js-cookie";
 
-import Footer from "../components/Footer"
-import Menu from "../components/Menu"
+import Footer from "../components/Footer";
+import Menu from "../components/Menu";
 
 interface ApiResponse {
-  message: string
+  message: string;
 }
 
 interface AccountInfoObject {
-  username: string
-  displayName: string
-  email: string
-  active: number
+  username: string;
+  displayName: string;
+  email: string;
+  active: number;
 }
 
 function isNullOrUndefined(value: any): boolean {
-  return value === null || value === undefined
+  return value === null || value === undefined;
 }
 
 async function getApiAccountInfo(username: string): Promise<ApiResponse> {
   try {
     const response = await axios.get(
       `http://localhost:4505/api/account/${username}?userinfo=true`
-    )
-    return response.data
+    );
+    return response.data;
   } catch (error) {
-    throw new Error("Can't get data")
+    throw new Error("Can't get data");
   }
 }
 
@@ -39,80 +39,84 @@ async function getApiSession(
   try {
     const response = await axios.get(
       `http://localhost:4505/api/account/${username}?token=${token}`
-    )
-    return response.data
+    );
+    return response.data;
   } catch (error) {
-    throw new Error("Can't get data")
+    throw new Error("Can't get data");
   }
 }
 
 function AccountInfo(): JSX.Element {
-  const navigate = useNavigate()
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
-  const [accountInfo, setAccountInfo] = useState<AccountInfoObject>()
+  const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [accountInfo, setAccountInfo] = useState<AccountInfoObject>();
 
-  const [displayName, setDisplayName] = useState<string>("")
-  const [email, setEmail] = useState<string>("")
-  const [password, setPassword] = useState<string>("")
-  const [newPassword, setNewPassword] = useState<string>("")
-  const [newPasswordAgain, setNewPasswordAgain] = useState<string>("")
+  const [displayName, setDisplayName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [newPassword, setNewPassword] = useState<string>("");
+  const [newPasswordAgain, setNewPasswordAgain] = useState<string>("");
 
   const handleLoggedIn = async (value: boolean) => {
-    setLoggedIn(value)
-    const username = Cookies.get("loginUsernameCookie")
+    setLoggedIn(value);
+    const username = Cookies.get("loginUsernameCookie");
     await axios.patch(`http://localhost:4505/api/account/${username}`, {
       active: -1,
       token: "",
-    })
-    Cookies.remove("loginTokenCookie")
-    Cookies.remove("loginUsernameCookie")
-    navigate("/")
-  }
+    });
+    Cookies.remove("loginTokenCookie");
+    Cookies.remove("loginUsernameCookie");
+    navigate("/");
+  };
 
   const handleSaveProfile = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    event.preventDefault();
 
     if (displayName !== "" || email !== "") {
-      const username = Cookies.get("loginUsernameCookie")
+      const username = Cookies.get("loginUsernameCookie");
       await axios.patch(`http://localhost:4505/api/account/${username}`, {
         active: -1,
         displayName: displayName,
-        email: email
-      })
-      alert("Thay đổi thông tin thành công!")
-      return
+        email: email,
+      });
+      alert("Thay đổi thông tin thành công!");
+      return;
     }
-    
-    alert("Vui lòng nhập đầy đủ thông tin cần thay đổi!")
-  }
 
-  const handleChangePassword = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    alert("Vui lòng nhập đầy đủ thông tin cần thay đổi!");
+  };
+
+  const handleChangePassword = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    event.preventDefault();
 
     if (password !== "" || newPassword !== "" || newPasswordAgain !== "") {
-      const username = Cookies.get("loginUsernameCookie")
-      await axios.get(`http://localhost:4505/api/account/checklogin/${username}?password=${password}`)
+      const username = Cookies.get("loginUsernameCookie");
+      await axios.get(
+        `http://localhost:4505/api/account/checklogin/${username}?password=${password}`
+      );
       if (newPassword === newPasswordAgain) {
         await axios.patch(`http://localhost:4505/api/account/${username}`, {
           active: -1,
-          password: newPassword
-        })
-        alert("Thay đổi mật khẩu thành công")
-        return
+          password: newPassword,
+        });
+        alert("Thay đổi mật khẩu thành công");
+        return;
       }
 
-      alert("Vui lòng nhập lại mật khẩu mới!")
-      return
+      alert("Vui lòng nhập lại mật khẩu mới!");
+      return;
     }
-    
-    alert("Vui lòng nhập đầy đủ thông tin cần thay đổi!")
-  }
+
+    alert("Vui lòng nhập đầy đủ thông tin cần thay đổi!");
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       // Get Login session for menu
-      const token = Cookies.get("loginTokenCookie")
-      const username = Cookies.get("loginUsernameCookie")
+      const token = Cookies.get("loginTokenCookie");
+      const username = Cookies.get("loginUsernameCookie");
 
       if (!isNullOrUndefined(token) || !isNullOrUndefined(username)) {
         getApiSession(username!, token!)
@@ -122,80 +126,128 @@ function AccountInfo(): JSX.Element {
             }
           })
           .catch((error: any) => {
-            console.log("Can't get data from api")
-            console.log(error)
-          })
+            console.log("Can't get data from api");
+            console.log(error);
+          });
 
         // Get account info
-        const accountinfo: any = await getApiAccountInfo(username!)
-        setAccountInfo(accountinfo)
-        setDisplayName(accountinfo.displayName)
-        setEmail(accountinfo.email)
+        const accountinfo: any = await getApiAccountInfo(username!);
+        setAccountInfo(accountinfo);
+        setDisplayName(accountinfo.displayName);
+        setEmail(accountinfo.email);
       } else {
-        navigate("/")
+        navigate("/");
       }
-    }
-    
-    fetchData()
-  }, [])
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="font-barlow">
       <Menu loggedIn={loggedIn} handleLoggedIn={handleLoggedIn} />
+      <div className="mt-4 py-4 w-full bg-grey-light text-coffee text-center text-2xl font-semibold ">
+        THÔNG TIN TÀI KHOẢN
+      </div>
+      <div>
+        {accountInfo !== null ? (
+          <div className="mx-auto mt-10 w-4/6 flex">
+            <div className="w-1/2">
+              <form onSubmit={handleSaveProfile}>
+                <div className="flex">
+                  <p className="w-1/3 font-semibold">Tên tài khoản:</p>
+                  <p> {accountInfo?.username}</p>
+                </div>
+                <div className="flex my-4">
+                  <p className="w-1/3 font-semibold">Tên hiển thị: </p>
+                  <input
+                    type="text"
+                    value={displayName}
+                    className="w-1/2 border-2 border-coffee px-1 bg-grey-light"
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setDisplayName(event.target.value)
+                    }
+                  />
+                </div>
 
-      <h1 className="text-center text-4xl">Thông tin tài khoản</h1>
-      {accountInfo !== null
-        ?
-        (
-          <div className="mx-auto w-4/5 flex">
-            <div>
-            <form className="flex-row" onSubmit={handleSaveProfile}>
-              <p>Tên tài khoản: {accountInfo?.username}</p>
-              <div>
-                <span>Tên hiển thị: </span>
-                <input type="text" value={displayName}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setDisplayName(event.target.value)} />
-              </div>
-              <div>
-                <span>Email: </span>
-                <input type="text" value={email}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)} />
-              </div>
-              <p>Xác thực tài khoản: {accountInfo?.active === 1 && "Đã kích hoạt"}</p>
-              <input type="submit" value="Lưu thay đổi"
-                className="mx-2 px-8 py-3 bg-coffee text-2xl text-white font-barlow" />
-            </form>
+                <div className="flex my-4">
+                  <p className="w-1/3 font-semibold">Email: </p>
+                  <input
+                    type="text"
+                    value={email}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setEmail(event.target.value)
+                    }
+                    className="w-1/2 border-2 border-coffee px-1 bg-grey-light"
+                  />
+                </div>
+
+                <div className="flex">
+                  <p className="w-1/3 font-semibold">Xác thực tài khoản:</p>
+                  <p> {accountInfo?.active === 1 && "Đã kích hoạt"}</p>
+                </div>
+
+                <input
+                  type="submit"
+                  value="Lưu thay đổi"
+                  className="px-4 mx-auto py-1 mt-4 bg-coffee text-xl text-white font-barlow block"
+                />
+              </form>
             </div>
-            <div>
-            <form onSubmit={handleChangePassword}>
-              <div>
-                <span>Nhập mật khẩu hiện tại: </span>
-                <input type="text" value={password}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setPassword(event.target.value)} />
-              </div>
-              <div>
-                <span>Nhập mật khẩu mới: </span>
-                <input type="text" value={newPassword}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNewPassword(event.target.value)} />
-              </div>
-              <div>
-                <span>Nhập lại mật khẩu mới: </span>
-                <input type="text" value={newPasswordAgain}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => setNewPasswordAgain(event.target.value)} />
-              </div>
-              <input type="submit" value="Thay đổi mật khẩu"
-                className="mx-2 px-8 py-3 bg-coffee text-2xl text-white font-barlow" />
-            </form>
+            <div className="w-1/5"></div>
+            <div className="w-1/2">
+              <form onSubmit={handleChangePassword}>
+                <div className="flex">
+                  <p className="w-2/3 font-semibold">
+                    Nhập mật khẩu hiện tại:{" "}
+                  </p>
+                  <input
+                    type="text"
+                    value={password}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setPassword(event.target.value)
+                    }
+                    className="w-1/2 border-2 border-coffee px-1 bg-grey-light"
+                  />
+                </div>
+                <div className="flex my-4">
+                  <p className="w-2/3 font-semibold">Nhập mật khẩu mới: </p>
+                  <input
+                    type="text"
+                    value={newPassword}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setNewPassword(event.target.value)
+                    }
+                    className="w-1/2 border-2 border-coffee px-1 bg-grey-light"
+                  />
+                </div>
+                <div className="flex">
+                  <p className="w-2/3 font-semibold">Nhập lại mật khẩu mới: </p>
+                  <input
+                    type="text"
+                    value={newPasswordAgain}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                      setNewPasswordAgain(event.target.value)
+                    }
+                    className="w-1/2 border-2 border-coffee px-1 bg-grey-light"
+                  />
+                </div>
+                <input
+                  type="submit"
+                  value="Thay đổi mật khẩu"
+                  className="px-4 mx-auto py-1 mt-14 bg-coffee text-xl text-white font-barlow block"
+                />
+              </form>
             </div>
           </div>
-        )
-        :
-        <div>Không tìm thấy dữ liệu</div>
-      }
+        ) : (
+          <div>Không tìm thấy dữ liệu</div>
+        )}
+      </div>
 
       <Footer />
     </div>
-  )
+  );
 }
 
-export default AccountInfo
+export default AccountInfo;
