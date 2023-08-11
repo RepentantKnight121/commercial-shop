5e6cb9bae14032b8893c047088965cadab9ac74c
Sex thủ si tềnh <127826830+Khoadz299@users.noreply.github.com>
2023-08-11 20:35:06 +0700

    NotFound Page (#30)
    * Sửa giao diện account info , payment
    
    * Thêm nút quay về cho Payment , Upadate Amout lên localStorage
    
    * Done UI NotFound

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,62 +1,74 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import Cookies from "js-cookie"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 
-import Menu from "../components/Menu"
+import Menu from "../components/Menu";
 
-import isNullOrUndefined from "../utils/check"
+import isNullOrUndefined from "../utils/check";
 
 interface ApiResponse {
-  message: string
+  message: string;
 }
 
-async function getApiSession(username: string, token: string): Promise<ApiResponse> {
+async function getApiSession(
+  username: string,
+  token: string
+): Promise<ApiResponse> {
   try {
-    const response = await axios.get(`http://localhost:4505/api/account/${username}?token=${token}`)
-    return response.data
+    const response = await axios.get(
+      `http://localhost:4505/api/account/${username}?token=${token}`
+    );
+    return response.data;
   } catch (error) {
-    throw new Error("Can't get data")
+    throw new Error("Can't get data");
   }
 }
 
 function NotFound(): JSX.Element {
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
 
   const handleLoggedIn = async (value: boolean) => {
-    setLoggedIn(value)
-    const username = Cookies.get("loginUsernameCookie")
+    setLoggedIn(value);
+    const username = Cookies.get("loginUsernameCookie");
     await axios.patch(`http://localhost:4505/api/account/${username}`, {
       active: -1,
-      token: ""
-    })
-    Cookies.remove("loginTokenCookie")
-    Cookies.remove("loginUsernameCookie")
-  }
+      token: "",
+    });
+    Cookies.remove("loginTokenCookie");
+    Cookies.remove("loginUsernameCookie");
+  };
 
   useEffect(() => {
-    const token = Cookies.get("loginTokenCookie")
-    const username = Cookies.get("loginUsernameCookie")
-    
+    const token = Cookies.get("loginTokenCookie");
+    const username = Cookies.get("loginUsernameCookie");
+
     if (!isNullOrUndefined(token) || !isNullOrUndefined(username)) {
       getApiSession(username!, token!)
         .then((user_data: any) => {
           if (user_data.session === token) {
-            setLoggedIn(true)
+            setLoggedIn(true);
           }
         })
         .catch((error: any) => {
-          console.log("Can't get data from api")
-          console.log(error)
+          console.log("Can't get data from api");
+          console.log(error);
         });
     }
-  }, [])
+  }, []);
 
   return (
-    <div>
+    <div className="font-barlow">
       <Menu loggedIn={loggedIn} handleLoggedIn={handleLoggedIn} />
 
-      <h1 className="text-center text-3xl">Không tìm thấy trang</h1>
-      <p className="text-center">Vui lòng thử lại trang khác</p>
+      <div className="pt-40 pb-10 mx-auto">
+        <h1 className="text-center text-6xl">Không tìm thấy trang</h1>
+        <p className="text-center text-xl mt-8">Vui lòng thử lại trang khác</p>
+        <a
+          href="/"
+          className="py-2 bg-blue-light block w-1/5 px-6 text-center mx-auto mt-10 hover:text-red-600">
+          Bấm vào đây để quay về trang chủ
+        </a>
+      </div>
     </div>
   );
 }
