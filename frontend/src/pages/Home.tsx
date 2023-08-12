c23786019b15296d511b13ed2496adb35416b9c9
Sex thủ si tềnh <khoa.ngkhoa26@gmail.com>
2023-08-13 00:08:47 +0700

    SlideShow

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,13 @@
-import axios from "axios"
-import { useState, useEffect } from "react"
-import Cookies from "js-cookie"
+import axios from "axios";
+import { useState, useEffect } from "react";
+import Cookies from "js-cookie";
 
-import Menu from "../components/Menu"
-import ListProductHomePage from "../components/ListProductHomePage"
+import Menu from "../components/Menu";
+import ListProductHomePage from "../components/ListProductHomePage";
 
-import isNullOrUndefined from "../utils/check"
-import { API_URL } from "../utils/URL"
+import isNullOrUndefined from "../utils/check";
+import { API_URL } from "../utils/URL";
+import Slideshow from "../components/SlideShow";
 
 interface ApiResponse {
   message: string;
@@ -19,35 +20,35 @@ async function getApiSession(
   try {
     const response = await axios.get(
       `${API_URL}/account/${username}?token=${token}`
-    )
-    return response.data
+    );
+    return response.data;
   } catch (error) {
-    throw new Error("Can't get data")
+    throw new Error("Can't get data");
   }
 }
 
 function Home(): JSX.Element {
-  const [loggedIn, setLoggedIn] = useState<boolean>(false)
-  const [productid, setProductId] = useState<string>("")
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+  const [productid, setProductId] = useState<string>("");
 
   const handleProductId = (value: string) => {
-    setProductId(value)
-  }
+    setProductId(value);
+  };
 
   const handleLoggedIn = async (value: boolean) => {
-    setLoggedIn(value)
-    const username = Cookies.get("loginUsernameCookie")
+    setLoggedIn(value);
+    const username = Cookies.get("loginUsernameCookie");
     await axios.patch(`${API_URL}/account/${username}`, {
       active: -1,
       token: "",
-    })
-    Cookies.remove("loginTokenCookie")
-    Cookies.remove("loginUsernameCookie")
-  }
+    });
+    Cookies.remove("loginTokenCookie");
+    Cookies.remove("loginUsernameCookie");
+  };
 
   useEffect(() => {
-    const token = Cookies.get("loginTokenCookie")
-    const username = Cookies.get("loginUsernameCookie")
+    const token = Cookies.get("loginTokenCookie");
+    const username = Cookies.get("loginUsernameCookie");
 
     if (!isNullOrUndefined(token) || !isNullOrUndefined(username)) {
       getApiSession(username!, token!)
@@ -57,23 +58,23 @@ function Home(): JSX.Element {
           }
         })
         .catch((error: any) => {
-          console.log("Can't get data from api")
-          console.log(error)
-        })
+          console.log("Can't get data from api");
+          console.log(error);
+        });
     }
-  }, [])
+  }, []);
 
   return (
     <div className="font-barlow">
       <Menu loggedIn={loggedIn} handleLoggedIn={handleLoggedIn} />
 
-      <img src="./src/assets/banner.png" alt="" className="w-10/12 mx-auto" />
+      <Slideshow />
 
       <p className="mt-10 text-center text-4xl font-semibold">SẢN PHẨM</p>
 
       <ListProductHomePage handleProductId={handleProductId} />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
